b23f11b4fc358276a55c3268526da2d3e361ee5c
felipe <felipemedina7878@gmail.com>
2020-11-24 23:18:16 -0500

    add do-while.html and js ,fixed arrowf.js

---
+++ primeros pasos js/arrowf.js
@@ -0,0 +1,54 @@
+
+var felipe =
+{
+    nombre: 'Felipe',
+    apellido:'Medina',
+    edad:16,
+    presidente: true,
+    cocinero:false,
+    cantante:false,
+    dj:false,
+    guitarrista:false,
+}
+var lina = 
+{
+    nombre: 'Lina',
+    apellido: 'Ospina',
+    edad: 18,
+    presidente: false,
+    cocinero:true,
+    cantante:true,
+    dj:false,
+    guitarrista:false,
+}
+
+const MAYOR_DE_EDAD = 18;
+
+const MAYORIA_DE_EDAD = ({edad})=> edad >= MAYOR_DE_EDAD
+
+const MENOR_DE_EDAD =({edad}) => edad <= MAYOR_DE_EDAD
+
+const cedula =(persona) =>{
+    if(MAYORIA_DE_EDAD(persona))
+    {
+        console.log(`${persona.nombre} es mayor de edad`)
+    }
+    else{
+        console.log(`${persona.nombre} no es mayor de edad`)
+    
+    }
+}
+cedula(lina);
+cedula(felipe);
+
+const permitiracceso = (persona) =>
+{
+    (!MAYORIA_DE_EDAD(persona))?
+    console.log("acceso denegado"):
+    console.log("acceso aprobado");
+  
+ 
+}
+
+permitiracceso(lina)
+permitiracceso(felipe)
---
+++ primeros pasos js/do-while.js
@@ -0,0 +1,10 @@
+console.log("hola")
+
+var contador = 0;
+var llueve = ()=> Math.random() <0.25;
+
+do {
+    contador++;
+} while (!llueve());  
+
+console.log(`fui a ver si llovia  ${contador} ${contador>1?"veces":"vez"}    `)
